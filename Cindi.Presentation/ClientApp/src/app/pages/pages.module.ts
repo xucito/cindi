bf72eb594134a6f4a609899ec4ef51ef988703e5
Tony Nguyen <tnguyen@etisalat.ae>
2019-07-27 14:43:43 +0400

    Change to starter project

--- Cindi.Presentation/ClientApp/src/app/pages/pages.module.ts
+++ Cindi.Presentation/ClientApp/src/app/pages/pages.module.ts
@@ -3,13 +3,15 @@ import { NbMenuModule } from '@nebular/theme';
 
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
+import { DashboardModule } from './dashboard/dashboard.module';
 import { PagesRoutingModule } from './pages-routing.module';
 
 @NgModule({
   imports: [
     PagesRoutingModule,
     ThemeModule,
-    NbMenuModule
+    NbMenuModule,
+    DashboardModule,
   ],
   declarations: [
     PagesComponent,
